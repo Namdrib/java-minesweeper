8f8cd5299dfe8d635a620401eaf96c47e0d2d7a7
Denton Phosavanh <dphosavanhau+github@gmail.com>
2018-09-26 14:00:04 +0930

    Add convert to maven project, add dummy test file to add test dir

---
+++ src/test/java/minesweeper/cell/CellImplTest.java
@@ -0,0 +1,107 @@
+package minesweeper.cell;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CellImplTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void testCellImpl() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testCellImplGamePointBoolean() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testAddListener() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testDeleteListener() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testAlertListeners() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testSetNumber() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetNumber() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testIsMine() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testSetPoint() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetPoint() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetGame() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testIsOpen() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetFlagState() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testGetCellState() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testSetMarking() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testToggleFlag() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testSetFlag() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testOpen() {
+    fail("Not yet implemented"); // TODO
+  }
+
+  @Test
+  public void testToString() {
+    fail("Not yet implemented"); // TODO
+  }
+
+}
